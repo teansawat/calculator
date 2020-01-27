efd456155bc0991f691ac774d8cfb0a69998bcec
teansawat <b.teansawat@gmail.com>
2020-01-27 15:54:00 +0700

    update(cal)
    update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+
+
+
+const routes: Routes = [
+  {
+    path : "" ,
+    component : CalculatorComponent
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
